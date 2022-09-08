60acd146cdf3b626974b00f1aade5e9693edb165
koenaramalepe <koenaramalepe2@gmail.com>
2022-09-08 14:34:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,34 @@
-#include <stdio.h>
+#!/bin/bash                                                                                                                             
 
-/**
-* main -print the string in the put function
-*
-*Description: using the main function
-*this program prints "Programming is like a building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return: 0;
+gcc -S $CFILE                                                                                                                           
+
+^C                                                                                                                                      
+
+root@0d331742006a:/alx-low_level_programming/0x00-hello_world# cat > 4-puts.c                                                           
+
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+
+* main -print the string in the put function                                                                                            
+
+*                                                                                                                                       
+
+*Description: using the main function                                                                                                   
+
+*this program prints "Programming is like a building a multilingual puzzle                                                              
+
+*Return: 0                                                                                                                              
+
+*/                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+        puts("\"Programming is like building a multilingual puzzle");                                                                   
+
+        return: (0);
 }
