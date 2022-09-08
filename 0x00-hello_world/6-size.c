1920ec758726692ef363bbf99fb08eed593a5229
koenaramalepe <koenaramalepe2@gmail.com>
2022-09-08 16:15:42 -0700

    hello puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+8this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int:%ld byte(s)\n", sizeof(i));
